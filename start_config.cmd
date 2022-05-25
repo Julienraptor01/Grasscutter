b1c186c42194e2de129a6f5835364b1640c38988
Julienraptor01 <julienraptor01@gmail.com>
2022-05-25 14:49:25 +0200

    TEMP

--- start_config.cmd
+++ start_config.cmd
@@ -12,14 +12,14 @@ set CUR_PATH=%~dp0
 @rem Executable Path
 @rem Note: Fill DO_NOT_CHECK_PATH if you need to run it from PATH
 @rem without detecting whether the executable file exists
-set JAVA_PATH=%JAVA_HOME%\bin\
-set MITMDUMP_PATH=%CUR_PATH%
-set MONGODB_PATH=%CUR_PATH%
+set JAVA_PATH=DO_NOT_CHECK_PATH
+set MITMDUMP_PATH=DO_NOT_CHECK_PATH
+set MONGODB_PATH=DO_NOT_CHECK_PATH
 
 @rem Utility Path
 set SERVER_JAR_PATH=%CUR_PATH%
 set DATABASE_STORAGE_PATH=%CUR_PATH%resources\Database
 
 @rem Utility Name
-set SERVER_JAR_NAME=grasscutter.jar
+set SERVER_JAR_NAME=grasscutter-1.1.2-dev.jar
 set PROXY_SCRIPT_NAME=proxy
